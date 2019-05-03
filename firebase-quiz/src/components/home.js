ca4a085937bdb9ec2c28d2b89c88ec9ed3de5086
vineelajallepalli <vineela.512@gmail.com>
2019-05-03 16:28:01 -0400

    blah

--- firebase-quiz/src/components/home.js
+++ firebase-quiz/src/components/home.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import '../css/home.css'
+<<<<<<< Updated upstream
 import { Provider } from 'react-redux'
 import Quiz from './quiz';
 import store from '../store/'
+=======
+import Firebase from 'firebase'
+import NavBar from "./NavBar"
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCrvSfTJuNJEUJjg1RW90NpqhKQ5s3SXFk",
+    authDomain: "g6qb-51732.firebaseapp.com",
+    databaseURL: "https://g6qb-51732.firebaseio.com",
+    projectId: "g6qb-51732",
+    storageBucket: "g6qb-51732.appspot.com",
+    messagingSenderId: "446672263519"
+  };
+
+  Firebase.initializeApp(config);
+
+  const Database = Firebase.database()
+>>>>>>> Stashed changes
 
 class Home extends React.Component {
     constructor(){
@@ -11,11 +30,18 @@ class Home extends React.Component {
 
     render(){
         return(
+<<<<<<< Updated upstream
             <Provider store={store}>
                 <div>
                     <Quiz/>
                 </div>
             </Provider>
+=======
+            <div>
+                <NavBar />
+                <center><u><h1>More Stuff Coming</h1></u></center>
+            </div>
+>>>>>>> Stashed changes
         )
     }
 }
--- firebase-quiz/src/index.js
+++ firebase-quiz/src/index.js
@@ -3,7 +3,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Home from './components/home';
 
+<<<<<<< Updated upstream
 ReactDOM.render(<App />, document.getElementById('root'));
+=======
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+>>>>>>> Stashed changes
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
