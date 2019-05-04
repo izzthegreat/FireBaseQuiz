831a98d7b0aabd57b5999c81256e7d2c7d03ade3
Bryant Allen <bryant.i.allen@gmail.com>
2019-05-04 17:59:59 -0400

    clear stash

--- firebase-quiz/src/index.js
+++ firebase-quiz/src/index.js
@@ -5,9 +5,6 @@ import AppRouter from './router';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
-ReactDOM.render(<Home />, document.getElementById('root'));
->>>>>>> Stashed changes
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
