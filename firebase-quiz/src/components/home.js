47d42c7640a3ae908993177d6341ee16e3255da0
vineelajallepalli <vineela.512@gmail.com>
2019-05-06 08:47:04 -0400

    Auto stash before merge of "master" and "origin/master"

--- firebase-quiz/src/components/home.js
+++ firebase-quiz/src/components/home.js
@@ -1,7 +1,33 @@
 import React from 'react'
 import '../css/home.css'
+<<<<<<< Updated upstream
 
 function Home () {
+=======
+import Firebase from 'firebase'
+import NavBar from './NavBar'
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCrvSfTJuNJEUJjg1RW90NpqhKQ5s3SXFk",
+    authDomain: "g6qb-51732.firebaseapp.com",
+    databaseURL: "https://g6qb-51732.firebaseio.com",
+    projectId: "g6qb-51732",
+    storageBucket: "g6qb-51732.appspot.com",
+    messagingSenderId: "446672263519"
+  };
+
+  Firebase.initializeApp(config);
+
+  const database = Firebase.database()
+
+class Home extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+>>>>>>> Stashed changes
         return(
          
             <div> 
