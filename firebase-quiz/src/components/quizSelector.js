706a5c8c348f540002e71c156b7223625ff047af
Bryant Allen <bryant.i.allen@gmail.com>
2019-05-09 10:21:58 -0400

    added delete

--- firebase-quiz/src/components/quizSelector.js
+++ firebase-quiz/src/components/quizSelector.js
@@ -10,19 +10,32 @@ class QuizSelector extends React.Component {
         super()
         this.state={
             quizNames:[],
+            deleteQuizPrompt: this.deleteQuizPrompt.bind(this),
+            deleteQuiz: this.deleteQuiz.bind(this)
         }
     }
 
     componentDidMount() {
         let data = database.ref('quizzes/quizNames/')
-        data.once('value', snapshot => {
+        data.on('value', snapshot => {
             let quizNames = snapshot.val()
             console.log(quizNames)
             this.setState({
                 quizNames: quizNames
             })
         })
+    }
 
+    deleteQuizPrompt(quiz){
+        if (window.confirm(`Are you sure you want to delete ${quiz}?`)){
+            this.state.deleteQuiz(quiz)
+            window.alert(`${quiz} has been deleted.`)
+        }   
+    }
+
+    deleteQuiz(quiz){
+        database.ref(`quizzes/quizNames/${quiz}`).set(null)
+        database.ref(`quizzes/${quiz}`).set(null)
     }
 
     render() {
@@ -42,6 +55,7 @@ class QuizSelector extends React.Component {
                                         {name}
                                     </Link>
                                     <p>{this.state.quizNames[name].desc}</p>
+                                    <input type='button' readOnly value='Delete' onClick={()=>this.state.deleteQuizPrompt(name)} />
                                 </div>
                             )
                         })}
