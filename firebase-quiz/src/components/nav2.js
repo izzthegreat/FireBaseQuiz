e6692bef51ba45452f0b677545446e8e983ab4aa
Bryant Allen <bryant.i.allen@gmail.com>
2019-05-09 14:43:51 -0400

    bugfixv2

--- firebase-quiz/src/components/nav2.js
+++ firebase-quiz/src/components/nav2.js
@@ -8,9 +8,10 @@ import {Navbar, Nav } from 'react-bootstrap'
 
 export default function Navigation (){
     return (
-<Navbar className = "navbar" bg="info" size = "lg" expand = "lg">
-        <Nav.Link className="link" href="/">Home</Nav.Link>
-        <Nav.Link className="link" href="/quizSelect">Take a Quiz!</Nav.Link>
-        <Nav.Link className="link" href="/editor">Make a quiz!</Nav.Link>
-</Navbar>
+        <Navbar className = "navbar" bg="info" size = "lg" expand = "lg">
+                <Nav.Link className="link" href="/">Home</Nav.Link>
+                <Nav.Link className="link" href="/quizSelect">Take a Quiz!</Nav.Link>
+                <Nav.Link className="link" href="/editor">Make a quiz!</Nav.Link>
+        </Navbar>
     )
+}
