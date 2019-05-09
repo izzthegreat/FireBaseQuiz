e21134686bf17a75a0d2f15e05ab8b1fc4fb8afc
Tiquila McNeill <tiquilamceille@gmail.com>
2019-05-08 23:57:15 -0400

    updated delete quiz code

--- firebase-quiz/src/components/deleteQuiz.js
+++ firebase-quiz/src/components/deleteQuiz.js
@@ -1,54 +1,48 @@
 import React from 'react';
-//import {database} from '../router'
-//import QuizSelector from './quizSelector'
-class QuizDel extends React.Component {
+import QuizSelector from './quizSelector'
+import { database } from 'firebase';
+class QuizDel extends React.Component{
     constructor(){
         super()
-        this.state={
-            quizNames:[],
-            selectedQuiz:undefined
+            this.state ={
+            quizNames: []    
+            }
+        this.deleteQuiz = this.deleteQuiz.bind(this)
+    }
+    componentDidMount(){
+        const quiz = database.ref(`quizzes/quizNames`)
+        quiz.once('value', snapshot => {
+            let quizNames = snapshot.val()
+            this.setState({
+                quiz: quizNames
+            })
+        })
+    }
+    deleteQuiz(name){
+        let newQuizArr = this.state.quizNames;
+        newQuizArr.map((quiz, index) => {if (name === quiz.name){
+            this.setState({deletedQiuzzes: [index, newQuizArr[index]]})
+            newQuizArr.splice(index,1)
         }
-    }
-    componentDidMount() {
-    const quiz = document.getElementById('name');
-    console.log(quiz)
-        // // Retrieve quiz info from Firebase
-        // const quiz = database.ref(`quizzes/quizNames/`)
-        // quiz.once('value', snapshot => {
-        //     let quizNames = snapshot.val()
-        //     this.setState({
-        //         quiz: quizNames
-        //         })
-        // })
-    }
-deleteQuiz(name){
-    let newQuizArr = this.state.quiz;
-    newQuizArr.map((quiz, index) => {
-        if(quiz === quiz.name){
-            this.setState({deletedQuizzes: [index, newQuizArr[index]]})
-            newQuizArr.splice(index, 1)
-        } else{console.log(quiz)}   
     });
-    this.setState({zw
-        quizNames: [],
-        deleted: true
-    })
-}
-
-   
+        this.setState({ 
+                quizNames: newQuizArr,
+                 deleted: true
+            
+        });
+    }
     render(){
         return(
-        <div>
-    <button className={`btn btn-danger sticky-top `} onClick={this.deleteQuiz.bind(this)}>Delete</button>
-    <div>
-       {this.state.newQuizArr}
-    </div>
-        </div>)
+            <div><p>
+            {this.state.quizNames}`
+             <button  className = {`btn btn-danger sticky-top ${this.state.deletedQiuzzes}`}onClick = {this.deleteQuiz}>Delete</button> 
+             </p>  
+            </div>
+        )
     }
 }
-   
-//quiz needs a unique id that can be removed or set = null
-//warning popup at the delete-- , button trigger alert, the then ok button triggers deletion, and a cancel--- confirmation box
+
+
 
 
 
