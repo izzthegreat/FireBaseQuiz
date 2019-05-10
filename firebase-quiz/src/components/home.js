94dea5675249f287e6fa1202464bea4fa50c4eb9
Tiquila McNeill <tiquilamceille@gmail.com>
2019-05-10 13:47:01 -0400

    images added, carosel added and a sound file

--- firebase-quiz/src/components/home.js
+++ firebase-quiz/src/components/home.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import '../css/home.css'
 import logo from '../Images/qblogo2.png'
-
+import BackgroundSlideshow from 'react-background-slideshow'
+import image1 from '../Images/bluesplash.jpg'
+import image2 from '../Images/bluemtn.jpg'
+import image3 from '../Images/blueflower.jpg'
+import image4 from '../Images/blueice.jpg'
+import image5 from '../Images/bluewave.jpg'
+import image6 from '../Images/mockaroon.jpg'
+import image7 from '../Images/icebluewave.jpg'
 function Home () {
 
     return(
          
             <div className ="homepage"> 
-                <main>
+                
+                <div className= "welogo">
                 Welcome to the Group 6 Quiz Bowl!!
                 <br />
-                <img src={logo} className = "app-logo" alt="qblogo"></img>
-                </main>
-            </div>
+                <img src={logo} className = "app-logo" alt="qblogo"/>
+                </div>
+                <div className ="BS">
+                <BackgroundSlideshow className ="BS" images={[ image1, image2, image3, image4, image5, image6, image7 ]} />
+                </div>
+            </div>   
         )
 }
 
